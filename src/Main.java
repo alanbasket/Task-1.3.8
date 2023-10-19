8dd8270a56ccd28edfa5cb3982f3662695dee0ef
baskaevalan <alanbasskaev002@gmail.com>
2023-10-19 20:34:18 +0300

    1.2.4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-    }
-
-    public static void calcCircleRadius(double area) {
-
-        double Pi = 3.14;
-        area = 5;
-        System.out.print(2 * Pi * area);
-
+        byte varbyte = 12;
+        short varshort = 56;
+        char varchar = 103;
+        int varint = 245;
+        long varlong = 1234L;
+        float varfloat = 1.23F;
+        double vardouble = 12.345;
+        boolean varboolean = true;
     }
 }
