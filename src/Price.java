9fed758ddf5089c68bb1b41ad7f55e9cd6a1dde7
baskaevalan <alanbasskaev002@gmail.com>
2023-10-28 21:23:25 +0300

    задача 1.2.7

---
+++ src/Price.java
@@ -0,0 +1,13 @@
+public class Price {
+        public static void main(String[] args) {
+            priceCalculation(2.3,12);
+        }
+        public static double priceCalculation(double price, int count) {
+            double result =price * count ;
+            System.out.println(result);
+            return result;
+
+        }
+    }
+
+
